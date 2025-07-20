132bb02d5b85c0ceb9ca5769b5b6653c5d2fbb7d
AngelRsm <kantoniainarasamimanana@yahoo.com>
2025-07-20 12:14:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const polaroids = document.querySelectorAll(".polaroid");
-  polaroids.forEach((img, index) => {
-    setTimeout(() => {
-      img.style.opacity = "1";
-      img.style.transform = "scale(1) translateY(0)";
-    }, index * 200); // stagger effect
-  });
-});
+
